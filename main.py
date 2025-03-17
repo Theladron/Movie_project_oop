f02d13f328a452a2b4beac949500f6e5854960c4
Theladron <Philwolframm@gmail.com>
2025-03-17 03:06:26 +0100

    Added all current methods of the MovieApp class, deleted obsolete files

---
+++ main.py
@@ -0,0 +1,11 @@
+from movie_app import MovieApp
+from storage_json import StorageJson
+
+
+def main():
+    storage = StorageJson('data.json')
+    movie_app = MovieApp(storage)
+    movie_app.run()
+
+if __name__ == "__main__":
+    main()
---
+++ movie_app.py
@@ -0,0 +1,262 @@
+import storage_json
+import user_input
+import random
+
+
+class MovieApp:
+    def __init__(self, storage):
+        self._storage = storage
+
+    def _command_exit_program(self):
+        """exits the program"""
+        print("bye!")
+        exit()
+
+    def _command_list_movies(self):
+        """
+        prints a list of all the movies with year and rating
+        """
+        movies = self._storage.list_movies()
+        movie_list = [f"{movie} ({movies[movie]["year"]})"
+                      f": {movies[movie]["rating"]}" for movie in movies]
+        print("\n".join(movie_list))
+
+
+    def _command_add_movie(self):
+        """
+        takes user input for a new movie, year and rating,
+        calls for saving the input
+         """
+        movies = self._storage.list_movies()
+        print("Enter the name of the movie: ", end="")
+        title = user_input.u_input("str")
+        for movie in movies:
+            if title.lower() == movie.lower():
+                print(f"The movie {title} already exists.")
+                return
+        print("Enter the year the movie was created: ", end="")
+        year = user_input.u_input("int")
+        print("Enter the movie rating: ", end="")
+        rating = user_input.add_exception("rating")
+        self._storage.add_movie(title, year, rating)
+        print(f"The movie '{title}' was added to the list.")
+
+
+    def _command_delete_movie(self):
+        """
+        takes user input for an existing movie,
+        calls for saving the changes
+        """
+        movies = self._storage.list_movies()
+        print("Enter the name of the movie: ", end="")
+        title = user_input.u_input("str")
+
+        for movie in movies:
+            if title.lower() == movie.lower():
+                self._storage.delete_movie(movie)
+                print(f"The movie '{movie}' was deleted.")
+                return
+        print(f"The movie {title} does not exist.")
+
+
+    def _command_update_movie(self):
+        """
+        takes user input for an existing movie and new rating,
+        calls for saving the input
+        """
+        movies = self._storage.list_movies()
+        print("Enter the name of the movie: ", end="")
+        title = user_input.u_input("str")
+        for movie in movies:
+            if title.lower() == movie.lower():
+                print("Enter the movie rating: ", end="")
+                rating = user_input.add_exception("rating")
+                self._storage.update_movie(movie, rating)
+                print(f"The movie '{title}' was updated")
+                return
+        print(f"The movie {title} does not exist.")
+
+
+    def _command_movie_stats(self):
+        """calls for average rating, median rating, best and worst movie """
+        movies = self._storage.list_movies()
+        rating_list = [movies[movie]["rating"] for movie in movies]
+        print("Movie statistics\n")
+        self._avg_rating(rating_list)
+        self._median_rating(rating_list)
+        self._best_movie(movies)
+        self._worst_movie(movies)
+
+
+    def _avg_rating(self, rating_list):
+        """prints average rating for the movie ratings"""
+        avg = sum(rating_list)
+        print(f"Average movie rating: "
+              f"{round(avg / len(rating_list), 1)}")
+
+    def _median_rating(self, rating_list):
+        """prints median rating for the movie ratings"""
+        rating_list.sort()
+        if len(rating_list) % 2 != 0:
+            print(f"Median movie rating: "
+                  f"{rating_list[round(len(rating_list) / 2)]}")
+        else:
+            print(f"Median movie rating: "
+                  f"{((rating_list[round(len(rating_list) / 2)]
+                       + rating_list[round(len(rating_list) / 2 + 1)]) / 2)}")
+
+    def _best_movie(self, movies):
+        """prints best movie by rating for the movie ratings"""
+        sort_movies = sorted(movies, key=lambda x: (-movies[x]["rating"], x))
+        check_val = 0
+        print("\nBest movie(s):")
+        for movie in sort_movies:
+            if movies[movie]["rating"] >= check_val:
+                print(f"{movie} ({movies[movie]["year"]}):"
+                      f" {movies[movie]["rating"]}")
+                check_val = movies[movie]["rating"]
+            else:
+                break
+
+    def _worst_movie(self, movies):
+        """prints worst movie by rating for the movie ratings"""
+        sort_movies = sorted(movies, key=lambda x: (movies[x]["rating"], x))
+        check_val = 10
+        print("\nWorst movie(s):")
+        for movie in sort_movies:
+            if movies[movie]["rating"] <= check_val:
+                print(f"{movie} ({movies[movie]["year"]}):"
+                      f" {movies[movie]["rating"]}")
+                check_val = movies[movie]["rating"]
+            else:
+                break
+
+    def _command_random_movie(self):
+        """prints a random movie with year and rating"""
+        movies = self._storage.list_movies()
+        r_number = random.randint(0, len(movies))
+        counter = 0
+        print("Your movie of choice is:")
+        for movie in movies:
+            if counter == r_number:
+                print(f"{movie} ({movies[movie]["year"]}): {movies[movie]["rating"]}")
+                break
+            counter += 1
+
+
+    def _command_search_movie(self):
+        """
+        takes user input, searches for movies that contain the user input,
+        prints out these movies with year and rating
+        """
+        print("Enter part of the movie you want to search for: ", end="")
+        movies = self._storage.list_movies()
+        user_search = user_input.u_input("str").lower()
+        movie_found = False
+        for movie in movies:
+            if user_search in movie.lower():
+                print(f"{movie} ({movies[movie]["year"]}): {movies[movie]["rating"]}")
+                movie_found = True
+        if not movie_found:
+            print(f"No movie was found for {user_search}")
+
+
+    def _command_sorted_by_rating(self):
+        """
+        sorts the movies by rating,
+        prints out movies, year and rating in descending order
+         """
+        movies = self._storage.list_movies()
+        sort_movies = sorted(movies, key=lambda x: (-movies[x]["rating"], x))
+        print("Movies sorted by rating:")
+        for movie in sort_movies:
+            print(f"{movie} ({movies[movie]["year"]}): {movies[movie]["rating"]}")
+
+
+    def _command_sorted_by_year(self):
+        """
+        sorts the movies by year,
+        prints out movies, year and rating in descending order
+        """
+        movies = self._storage.list_movies()
+        sort_movies = sorted(movies, key=lambda x: (-movies[x]["year"], x))
+        print("Movies sorted by year:")
+        for movie in sort_movies:
+            print(f"{movie} ({movies[movie]["year"]}): {movies[movie]["rating"]}")
+
+
+    def _command_filter_movies(self):
+        """
+        Takes user input for minimum rating, start and end year,
+        prints the movies matching the users criteria
+        """
+        movies = self._storage.list_movies()
+        print(f"Enter minimum rating (leave "
+              f"blank for no minimum rating): ", end="")
+        min_rat = user_input.add_exception("floatrange")
+        print(f"Enter start year "
+              f"(leave blank for no start year): ", end="")
+        start = user_input.add_exception("range")
+        print("Enter end year "
+              "(leave blank for no end year): ", end="")
+        end = user_input.add_exception("range")
+        print("Movies that match your criteria:")
+        if not min_rat:
+            min_rat = 0
+        if not start:
+            start = 0
+        if not end:
+            for movie in movies:
+                if (movies[movie]["rating"] >= min_rat
+                        and movies[movie]["year"] >= start):
+                    print(f"{movie} ({movies[movie]["year"]}): {movies[movie]["rating"]}")
+        else:
+            for movie in movies:
+                if (movies[movie]["rating"] >= min_rat
+                        and start <= movies[movie]["year"] <= end):
+                    print(f"{movie} ({movies[movie]["year"]}): {movies[movie]["rating"]}")
+
+
+    def _generate_website(self):
+        pass
+
+    def run(self):
+        funct_dict = {
+            0: self._command_exit_program,
+            1: self._command_list_movies,
+            2: self._command_add_movie,
+            3: self._command_delete_movie,
+            4: self._command_update_movie,
+            5: self._command_movie_stats,
+            6: self._command_random_movie,
+            7: self._command_search_movie,
+            8: self._command_sorted_by_rating,
+            9: self._command_sorted_by_year,
+            10: self._command_filter_movies
+        }
+        while True:
+            print("""********** My Movies Database **********
+
+            Menu:
+            0. Exit
+            1. List movies
+            2. Add movie
+            3. Delete movie
+            4. Update movie
+            5. Stats
+            6. Random movie
+            7. Search movie
+            8. Movies sorted by rating
+            9. Movies sorted by year
+            10. Filter movies
+
+                Enter choice (1-10): """, end="")
+            u_input = user_input.add_exception("menu")
+            if not self._storage.list_movies():
+                if not (u_input == 2 or u_input == 0):
+                    print("Error. The movie list is empty. Please"
+                          " enter '2' and add a movie to the list.")
+                    input("Press enter to continue...")
+                    continue
+            funct_dict[u_input]()
+            input("\nPress enter to continue...")
