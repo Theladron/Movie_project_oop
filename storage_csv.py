25dec972441200da4bd438076f34a39de6d11ec9
Theladron <Philwolframm@gmail.com>
2025-03-19 03:18:01 +0100

    Added StorageCsv class

---
+++ storage_csv.py
@@ -0,0 +1,67 @@
+from istorage import IStorage
+import json
+
+class StorageCsv(IStorage):
+
+    def __init__(self, file_path):
+        self._file_path = file_path
+
+    def list_movies(self):
+        """
+        prints a list of all the movies in the JSON file,
+        with year and rating
+        """
+        with open(self._file_path, "r") as handle:
+            cvs_data = handle.readlines()
+        data = {}
+        for line in cvs_data[1:]:
+            data_elements = line.split(",")
+            data[data_elements[0]] = {"year" : int(data_elements[1]),
+                                      "rating" : float(data_elements[2][:-1])}
+        return data
+
+
+    def add_movie(self, title, year, rating):
+        """
+        takes user input for a new movie, year and rating,
+        calls for saving the input to the JSON File
+        """
+        with open(self._file_path, "r") as handle:
+            movies = handle.read()
+
+        movies += f"{title},{year},{rating}\n"
+
+        with open(self._file_path, "w") as handle:
+            handle.write(movies)
+
+    def delete_movie(self, title):
+        """
+        takes user input for an existing movie,
+        calls for saving the changes in the JSON File
+        """
+        with open(self._file_path, "r") as handle:
+            movies = handle.readlines()
+        new_movies = ""
+        for line in movies:
+            movie_elements = line.split(",")
+            if title.lower() != movie_elements[0].lower():
+                new_movies += line
+        with open(self._file_path, "w") as handle:
+            handle.write(new_movies)
+
+
+    def update_movie(self, title, rating):
+        """
+        takes user input for an existing movie and new rating,
+        calls for saving the input to the JSON File
+        """
+        with open(self._file_path, "r") as handle:
+            movies = handle.readlines()
+        new_movies = ""
+        for line in movies:
+            movie_elements = line.split(",")
+            if title.lower() == movie_elements[0].lower():
+                line = line.replace(movie_elements[2], str(rating)+"\n")
+            new_movies += line
+        with open(self._file_path, "w") as handle:
+            handle.write(new_movies)
